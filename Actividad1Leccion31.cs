0b96cfdce220c74294f80c1b3804d8dafc7282d1
22ACM01-AlexCalderon <a.calderon.6ddb@gmail.com>
2026-07-07 23:03:54 -0600

    Add Program class to handle 3x5 matrix input/output

---
+++ Actividad1Leccion31.cs
@@ -0,0 +1,27 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+      int filas = 3;
+      int columnas = 5;
+      int i=0; int j=0;
+      int [,]matriz = new int [3,5];
+      
+      for (i = 0; i < filas; i++)
+      {
+          for (j = 0; j < columnas; j++)
+          {
+              Console.WriteLine("Ingresa el numero que quieres guardar en la fila: " +i+", columna: " +j+" =");
+              matriz[i,j] = int.Parse(Console.ReadLine());
+          }
+      }
+      
+      for (i = 0; i < filas; i++)
+      {
+          for (j = 0; j < columnas; j++)
+          {
+              Console.WriteLine("Fila: " +i+", Columna: " +j+" =" +matriz[i,j]);
+          }
+      }
+    }
+}
