960efe9428eb6d5128b1a0e4851aeca12455b6b4
22ACM01-AlexCalderon <a.calderon.6ddb@gmail.com>
2026-07-07 23:08:03 -0600

    Add program to find the largest number in a matrix

---
+++ Actividad3-Leccion17-Eje2.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int filas = 4;
+        int columnas = 4;
+        int[,] tabla = new int[filas, columnas];
+
+        for (int fila = 0; fila < filas; fila++)
+        {
+            for (int columna = 0; columna < columnas; columna++)
+            {
+                Console.Write("Ingresa el valor para la fila " + (fila + 1) + ", columna " + (columna + 1) + ": ");
+                tabla[fila, columna] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        int mayor = tabla[0, 0];
+        int filaMayor = 0;
+        int columnaMayor = 0;
+
+        for (int fila = 0; fila < filas; fila++)
+        {
+            for (int columna = 0; columna < columnas; columna++)
+            {
+                if (tabla[fila, columna] > mayor)
+                {
+                    mayor = tabla[fila, columna];
+                    filaMayor = fila;
+                    columnaMayor = columna;
+                }
+            }
+        }
+
+        Console.WriteLine("El numero mayor es: " + mayor);
+        Console.WriteLine("Se encuentra en la fila: " + (filaMayor + 1) + " y columna: " + (columnaMayor + 1));
+    }
+}
